90d46a34047cb47194bfeb50c96eba844cfdbdcc
Tyler Hoffman <tyhoffman1@gmail.com>
2013-12-03 21:30:00 -0500

    Moved the gif directory to ~/timelapse/static/camera/gifs

--- makegif.sh
+++ makegif.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 date=`date +"%Y-%m-%d--%H"`
-convert -delay 10 -loop 0 ~/timelapse/timelapse/static/camera/$date* ~/timelapse/gifs/$date.gif
+convert -delay 10 -loop 0 ~/timelapse/timelapse/static/camera/$date* ~/timelapse/static/camera/gifs/$date.gif
